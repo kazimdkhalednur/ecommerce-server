1a066c5ed0321ee690718120c75016053de538e8
Kazi Khaled Nur <kazikhalednur@gmail.com>
2023-06-28 18:39:00 +0600

    Initial Commit

---
+++ root/local_settings.py
@@ -0,0 +1,10 @@
+from django.utils.timezone import timedelta
+
+from .settings import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
